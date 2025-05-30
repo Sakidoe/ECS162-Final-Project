5774618686315b11482adfea75735aa3f241442e
anorak-lightman <164287745+anorak-lightman@users.noreply.github.com>
2025-05-30 12:41:32 -0700

    Initial Commit/App template

---
+++ Final Project/app/frontend/src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let apiKey: string = '';
+
+//   onMount(async () => {
+//     try {
+//       const res = await fetch('/api/key');
+//       const data = await res.json();
+//       apiKey = data.apiKey;
+//     } catch (error) {
+//       console.error('Failed to fetch API key:', error);
+//     }
+//   }); 
+</script>
+
+<main>
+    
+</main>
