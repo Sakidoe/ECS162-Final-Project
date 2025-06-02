0da83f572d889a9d733b9ca63d1b591006daf4cc
Jonathan Ngo <jonngo@ucdavis.edu>
2025-06-01 19:15:32 -0700

    skeleton done

--- Final Project/app/frontend/src/task.svelte
+++ Final Project/app/frontend/src/task.svelte
@@ -1,5 +1,15 @@
 <script>
+    let checked=false;
+    let allTags = [
+        { name: 'school', checked: false },
+        { name: 'work', checked: false },
+        { name: 'tag', checked: false },
+        { name: 'personal', checked: false },
+        { name: 'tag', checked: false },
+        { name: 'tag', checked: false }
+    ];
 
+    let selectedTags = [];
 </script>
 
 <style>
@@ -13,7 +23,7 @@
         background-color: lightgrey;
         display: flex;
         flex-direction: column;
-        padding: 24px;
+        padding: 48px;
     }
 
     .inputBox {
@@ -29,13 +39,22 @@
         display: block;
         margin-top: 16px;
         width: 100%; 
-        height: 40%;             
+        height: 30%;             
         padding: 16px;
         background-color: lightblue;
         border: none;
         border-radius: 8px;
         box-sizing: border-box;
     }
+
+    .tagContainer {
+        margin:16px;
+        padding: 16px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content:space-between;
+    }
+
 </style>
 
 <main>
@@ -49,6 +68,34 @@
         <textarea
             class="commentBox"
             placeholder="enter any comments, or task description"
+            required
+        />
+        <h3>tags:</h3>
+        <div class="tagContainer">
+            {#each allTags as tag}
+                <label>
+                    <input
+                        class="tagItem"
+                        type="checkbox"
+                        bind:checked={tag.checked}
+                        on:change={() => {
+                            if (tag.checked) {
+                                selectedTags.push(tag.name);
+                            } else {
+                                selectedTags = selectedTags.filter(
+                                    (t) => t !== tag.name
+                                );
+                            }
+                        }}
+                    />
+                    {tag.name}
+                </label>
+            {/each}
+        </div>
+        <input
+            class="inputBox"
+            type="date"
+            placeholder="enter due date here"
             required
         />
     </div>
